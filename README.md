fbb23e71ed27808029a2164a7403f17f0bfa9e0f
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2024-08-25 16:23:57 +0530

    Update Readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # nft-passport
 
 Sample NFT Passport App
+
+You can download the apk from link below
+https://we.tl/t-xlXSkIo6E3
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'module:react-native-dotenv',
-    {
-      envName: 'APP_ENV',
-      moduleName: '@env',
-      path: '.env',
-      blocklist: null,
-      allowlist: null,
-      blacklist: null, // DEPRECATED
-      whitelist: null, // DEPRECATED
-      safe: false,
-      allowUndefined: true,
-      verbose: false,
-    },
-    ,
-    'react-native-reanimated/plugin',
-  ],
+  plugins: ['module:react-native-dotenv', 'react-native-reanimated/plugin'],
 };
