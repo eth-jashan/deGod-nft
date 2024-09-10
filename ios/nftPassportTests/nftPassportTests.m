e0fddb5e928fe7a8619fa9339ef7c4011cb33e24
Jashan Shetty <43118727+eth-jashan@users.noreply.github.com>
2024-09-11 00:28:31 +0530

    WIP Project change

--- ios/nftPassportTests/nftPassportTests.m
+++ ios/nftPassportTests/nftPassportTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface nftPassportTests : XCTestCase
+@interface xyzAiTests : XCTestCase
 
 @end
 
-@implementation nftPassportTests
+@implementation xyzAiTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
